c8bf9df00bfcf698c72f5a6512d67313e6cfe131
Prachi-6921 <prachi02rani@gmail.com>
2026-03-30 14:46:24 +0530

    [Prachi Rani] added uc5:Display Hello with Multiple Command-Line Arguments using Enhanced For Loop or Default Message

--- HelloApp1/src/main/java/org/example/HelloApp1.java
+++ HelloApp1/src/main/java/org/example/HelloApp1.java
@@ -4,31 +4,20 @@ public class HelloApp1 {
 
     public static void main(String[] args) {
 
-        String message;
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
 
-        // Check if any command-line arguments were provided
-        if (args.length > 0) {
+        StringBuilder names = new StringBuilder();
 
-            StringBuilder names = new StringBuilder();
-
-            // Loop through all arguments
-            for (int i = 0; i < args.length; i++) {
-                names.append(args[i]);
-
-                // Add comma and space between names
-                if (i < args.length - 1) {
-                    names.append(", ");
-                }
+        for (String name : args) {
+            if (names.length() > 0) {
+                names.append(", ");
             }
-
-            message = "Hello, " + names.toString() + "!";
-        }
-        else {
-            // Default message when no arguments are given
-            message = "Hello, World!";
+            names.append(name);
         }
 
-        // Display greeting
-        System.out.println(message);
+        System.out.println("Hello, " + names + "!");
     }
 }
